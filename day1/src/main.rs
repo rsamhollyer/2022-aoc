67758719269bc3b54a05eeb89a8e8a0ffaa94c54
Sam Hollyer <rsamhollyer@gmail.com>
2023-04-16 18:21:11 -0500

    sum imperatively

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,8 +1,20 @@
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
 
-    for line in include_str!("./input.txt").lines() {
-        println!("Line: {line}");
+    for group in include_str!("./input.txt")
+        .replace("\r\n", "\n")
+        .split("\n\n")
+    {
+        println!("In a group");
+
+        let mut sum = 0;
+
+        for line in group.lines() {
+            let value = line.parse::<u64>()?;
+            println!(" - {value}");
+            sum += value;
+        }
+        println!("Sum: {sum}");
     }
 
     Ok(())
