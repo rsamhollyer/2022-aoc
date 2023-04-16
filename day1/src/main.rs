d77e117dca2c38002585b60b248982079c80eea3
Sam Hollyer <rsamhollyer@gmail.com>
2023-04-16 18:05:33 -0500

    read input

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use color_eyre::eyre::Context;
+
+fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+
+    let input = read_input()?;
+
+    println!("{input}");
+    Ok(())
+}
+
+fn read_input() -> color_eyre::Result<String> {
+    let input: String = std::fs::read_to_string("./input.txt").wrap_err("reading input.txt")?;
+    Ok(input)
 }
